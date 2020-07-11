d50d529fcaa2765ea230db375ee8b3e23f8833be
Théo Vidal <exybore@becauseofprog.fr>
2020-07-11 18:58:49 +0200

    feat: lock bot commands to owner-only

--- main.rb
+++ main.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'telegram/bot'
 require 'psych'
 require 'time'
@@ -8,18 +10,18 @@ require_relative 'lib/command'
 require_relative 'lib/bot'
 
 # Loading config file
-config_file = File.dirname(__FILE__) + "/config.yml"
-raise StandardError, "The configuration file is required" unless File::exist?(config_file)
+config_file = File.dirname(__FILE__) + '/config.yml'
+raise StandardError, 'The configuration file is required' unless File.exist?(config_file)
+
 config = Psych.load(File.read(config_file))
 
 # Dynamically loading commands
-commands = {}
-base = 'commands'
-Dir["#{base}/*.rb"].each do |path|
+commands_dir = 'commands'
+Dir["#{commands_dir}/*.rb"].each do |path|
   resp = Regexp.new("([a-z0-9]+)\.rb$").match(path)
-  if resp != nil
-    cmd_name = resp[1]
-    require_relative "#{base}/#{cmd_name}"
+  unless resp.nil?
+    command_name = resp[1]
+    require_relative "#{commands_dir}/#{command_name}"
   end
 end
 
@@ -27,13 +29,22 @@ Telegram::Bot::Client.run(config['bot']['token']) do |bot|
   puts 'Telegram bot running.'
 
   client = Raspexy::Bot.new(bot, config)
-  bot.listen do |message|
-    case message
+  bot.listen do |event|
+    unless event.from.id == config['bot']['owner'].to_i
+      bot.api.send_message(
+        chat_id: event.chat.id,
+        text: "🚫 Vous n'avez pas la permission d'intéragir avec le robot."
+      )
+      next
+    end
+
+    case event
     when Telegram::Bot::Types::Message
-      next unless message.text[0] == '/'
-      client.run_command(message)
+      next unless event.text[0] == '/'
+
+      client.run_command(event)
     when Telegram::Bot::Types::CallbackQuery
-      client.run_command(message, true)
+      client.run_command(event, true)
     end
   end
 end
